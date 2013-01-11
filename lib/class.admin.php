a53f2f11bea3fc4daf65d1e6f647701ebf5928e2
Ronald Huereca <ronalfy@gmail.com>
2013-01-11 15:49:09 +0100

    Removing getting started page.
    Signed-off-by: Ronald Huereca <ronalfy@gmail.com>

--- lib/class.admin.php
+++ lib/class.admin.php
@@ -7,10 +7,6 @@ class AECAdmin {
 			add_menu_page( 'Ajax Edit Comments', 'AEC', $capabilities, 'wpaec', array("AECAdmin", 'print_admin_page'), $aecomments->get_plugin_url( 'images/menu-icon.png' ) );
 			do_action('aec-addon-menus');
 		}
-		public static function print_getting_started() {
-			global $aecomments;
-			include_once $aecomments->get_plugin_dir( '/views/admin-getting-started.php' );
-		}
 		
 		//Provides the interface for the admin pages
 		public static function print_admin_page() {
