1174a13599e3b57844781f6a810bab49797d22d9
Mikhail Kobzarev <mihdan@yandex.ru>
2020-01-13 01:49:57 +0300

    Fix bug "backslashes removed after editing comments"

--- js/comment-editor.js
+++ js/comment-editor.js
@@ -116,112 +116,131 @@
 	    $j("#title a").bind("click", function() { window.open(this.href); return false; } );
 	    //Save button event
 	  }
+
 		function load_comment() {
 
-	    //Change the edit text and events
-	    $j("#status").show();
-	    $j("#status").attr("class", "success");
-	    $j("#message").html(wpajaxeditcommentedit.AEC_Loading);
+			//Change the edit text and events
+			$j( '#status' ).show();
+			$j( '#status' ).attr( 'class', 'success' );
+			$j( '#message' ).html( wpajaxeditcommentedit.AEC_Loading );
 
-		jQuery.post( ajaxurl, { action: $j("#action").attr("value"), cid: parseInt($j("#commentID").attr("value")) ,pid: parseInt($j("#postID").attr("value")), _ajax_nonce: $j('#_wpnonce').val() },
-	function(data){
-			//Add event for save button
+			jQuery.post( ajaxurl, {
+					action: $j( '#action' ).attr( 'value' ),
+					cid: parseInt( $j( '#commentID' ).attr( 'value' ) ),
+					pid: parseInt( $j( '#postID' ).attr( 'value' ) ),
+					_ajax_nonce: $j( '#_wpnonce' ).val()
+				},
+				function ( data ) {
+					//Add event for save button
+					var error = false;
+					$j( '#save' ).bind( 'click', function () {
+						save_comment();
+						return false;
+					} );
+					if ( typeof data.error != 'undefined' ) { //error
+						error = true;
+						$j( '#status' ).attr( 'class', 'error' );
+						$j( '#message' ).html( data.error );
+						$j( '#close-option' ).show();
+						//remove event for save button
+						$j( '#save' ).unbind( 'click' );
+					} else { //success
+						//Load content
+						$j( '#comment' ).html( data.comment_content ); //For everyone else
+						//$j( '#comment' ).attr( 'value', data.comment_content ); //For Opera
+						$j( '#name' ).attr( 'value', data.comment_author );
+						$j( '#e-mail' ).attr( 'value', data.comment_author_email );
+						$j( '#URL' ).attr( 'value', data.comment_author_url );
+					}
+					if ( !error ) {
+						//Enable the buttons
+						$j( '#save, #cancel, #check_spelling' ).removeAttr( 'disabled' );
+						//Update status message
+						$j( '#status' ).attr( 'class', 'success' );
+						$j( '#message' ).html( wpajaxeditcommentedit.AEC_LoadSuccessful );
+					}
+				}, 'json' );
+		} //end load_comment
+		function save_comment() {
+			//After the deadline
+			if ( wpajaxeditcommentedit.AEC_AftertheDeadline == 'true' ) {
+				$j( '.AtD_edit_button' ).trigger( 'click' );
+			}
+			//Update status message
+			$j( '#status' ).attr( 'class', 'success' );
+			$j( '#message' ).html( wpajaxeditcommentedit.AEC_Saving );
+			$j( '#save' ).attr( 'disabled', 'disabled' );
 			var error = false;
-			$j("#save").bind("click", function() { save_comment(); return false; });
-			if ( typeof data.error != "undefined" ) { //error
-				error = true;
-				$j("#status").attr("class", "error");
-				$j("#message").html( data.error );
-				$j("#close-option").show();
-				//remove event for save button
-				$j("#save").unbind("click");
-			} else { //success
-				//Load content
-				$j("#comment").html( data.comment_content ); //For everyone else
-				$j("#comment").attr("value", data.comment_content  ); //For Opera
-				$j("#name").attr("value", data.comment_author);
-				$j("#e-mail").attr("value", data.comment_author_email);
-				$j("#URL").attr("value", data.comment_author_url);
+			//Read in dom values
+			var name = encodeURIComponent( $j( '#name' ).attr( 'value' ) );
+			var email = encodeURIComponent( $j( '#e-mail' ).attr( 'value' ) );
+			var url = encodeURIComponent( $j( '#URL' ).attr( 'value' ) );
+			var comment = $j( '#comment' ).val();
+			var nonce = $j( '#_wpnonce' ).attr( 'value' );
+			var data = {
+				action: 'savecomment',
+				cid: parseInt( $j( '#commentID' ).attr( 'value' ) ),
+				pid: parseInt( $j( '#postID' ).attr( 'value' ) ),
+				_ajax_nonce: $j( '#_wpnonce' ).val(),
+				comment_content: comment,
+				comment_author: name,
+				comment_author_email: email,
+				comment_author_url: url
+			};
+			//Comment Status
+			if ( $j( '#comment-status-radio' ).length > 0 ) {
+				var comment_status = $j( '#comment-status-radio :checked' ).attr( 'value' );
+				data = $j.extend( data, { comment_status: comment_status } );
 			}
-			if (!error) {
-				//Enable the buttons
-				$j("#save, #cancel, #check_spelling").removeAttr("disabled");
-				//Update status message
-				$j("#status").attr("class", "success");
-				$j("#message").html(wpajaxeditcommentedit.AEC_LoadSuccessful);
+			//Timestamp
+			if ( $j( '#timestamp' ).length > 0 ) {
+				var month = encodeURIComponent( $j( '#mm' ).attr( 'value' ) );
+				var day = encodeURIComponent( $j( '#jj' ).attr( 'value' ) );
+				var year = encodeURIComponent( $j( '#aa' ).attr( 'value' ) );
+				var hour = encodeURIComponent( $j( '#hh' ).attr( 'value' ) );
+				var minute = encodeURIComponent( $j( '#mn' ).attr( 'value' ) );
+				var ss = encodeURIComponent( $j( '#ss' ).attr( 'value' ) );
+				data = $j.extend( data, { mm: month, jj: day, aa: year, hh: hour, mn: minute, ss: ss } );
 			}
-	}, "json" );
-	  } //end load_comment
-	  function save_comment() {
-		//After the deadline
-		 if (wpajaxeditcommentedit.AEC_AftertheDeadline == 'true') {
-			 $j(".AtD_edit_button").trigger("click");
-		 }
-	    //Update status message
-	    $j("#status").attr("class", "success");
-	    $j("#message").html(wpajaxeditcommentedit.AEC_Saving);
-	    $j("#save").attr("disabled", "disabled");
-	    var error = false;
-	    //Read in dom values
-	    var name = encodeURIComponent($j("#name").attr("value"));
-	    var email = encodeURIComponent($j("#e-mail").attr("value"));
-	    var url = encodeURIComponent($j("#URL").attr("value"));
-	    var comment = encodeURIComponent($j("#comment").attr("value"));
-	    var nonce = $j("#_wpnonce").attr("value");
-		var data = {	action: "savecomment", cid: parseInt($j("#commentID").attr("value")) ,pid: parseInt($j("#postID").attr("value")), _ajax_nonce: $j('#_wpnonce').val(), comment_content: comment, comment_author: name, comment_author_email: email, comment_author_url: url };
-		//Comment Status
-		if ($j("#comment-status-radio").length > 0) {
-			var comment_status = $j("#comment-status-radio :checked").attr("value");
-			data = $j.extend(data, { comment_status: comment_status});
-		}
-		//Timestamp
-		if ($j("#timestamp").length > 0) {
-			var month = encodeURIComponent($j("#mm").attr("value"));
-			var day = encodeURIComponent($j("#jj").attr("value"));
-			var year = encodeURIComponent($j("#aa").attr("value"));
-			var hour = encodeURIComponent($j("#hh").attr("value"));
-			var minute = encodeURIComponent($j("#mn").attr("value"));
-			var ss = encodeURIComponent($j("#ss").attr("value"));
-			data = $j.extend(data, { mm: month, jj: day, aa: year, hh: hour,mn: minute,ss:ss});
-		}
-		jQuery.post( ajaxurl, data,
-	function ( response ) {
-		if ( typeof response.error != "undefined" ) { //error
-			error = true;
-			$j("#save").removeAttr("disabled");
-			$j("#status").attr("class", "error");
-			$j("#message").html( response.error );
-			$j("#close-option").show();
-		} else { //success
-			var comment = response.content;
-			var name = response.comment_author;
-			var url = response.comment_author_url;
-			var date = response.comment_date;
-			var time = response.comment_time;
-			var undo = response.undo;
-			var comment_approved = response.comment_approved;
-			var old_comment_approved = response.old_comment_approved;
-			var spam_count = response.spam_count;
-			var moderation_count = response.moderation_count;
-			var comment_links = response.comment_links;
-		}
-		if (!error) {
-		try {
-			self.parent.jQuery.ajaxeditcomments.update_comment("edit-comment" + data.cid,comment);
-			self.parent.jQuery.ajaxeditcomments.update_author("edit-author" + data.cid,name, url);
-			self.parent.jQuery.ajaxeditcomments.update_date_or_time("aecdate" + data.cid,date);
-			self.parent.jQuery.ajaxeditcomments.undo_message(data.cid, undo,true);
-			self.parent.jQuery(".spam-count").html(spam_count);
-			self.parent.jQuery("#edit-comment-admin-links" + data.cid).html(comment_links);
-			self.parent.jQuery(".pending-count").html(moderation_count);
-		} catch (err) {}
-		$j("#status").attr("class", "success");
-		$j("#message").html(wpajaxeditcommentedit.AEC_Saved);
-		 parent.jQuery.fn.colorbox.close();
-		}
-																																																			 }, "json" );
+			jQuery.post( ajaxurl, data,
+				function ( response ) {
+					if ( typeof response.error != 'undefined' ) { //error
+						error = true;
+						$j( '#save' ).removeAttr( 'disabled' );
+						$j( '#status' ).attr( 'class', 'error' );
+						$j( '#message' ).html( response.error );
+						$j( '#close-option' ).show();
+					} else { //success
+						var comment = response.content;
+						var name = response.comment_author;
+						var url = response.comment_author_url;
+						var date = response.comment_date;
+						var time = response.comment_time;
+						var undo = response.undo;
+						var comment_approved = response.comment_approved;
+						var old_comment_approved = response.old_comment_approved;
+						var spam_count = response.spam_count;
+						var moderation_count = response.moderation_count;
+						var comment_links = response.comment_links;
+					}
+					if ( !error ) {
+						try {
+							self.parent.jQuery.ajaxeditcomments.update_comment( 'edit-comment' + data.cid, comment );
+							self.parent.jQuery.ajaxeditcomments.update_author( 'edit-author' + data.cid, name, url );
+							self.parent.jQuery.ajaxeditcomments.update_date_or_time( 'aecdate' + data.cid, date );
+							self.parent.jQuery.ajaxeditcomments.undo_message( data.cid, undo, true );
+							self.parent.jQuery( '.spam-count' ).html( spam_count );
+							self.parent.jQuery( '#edit-comment-admin-links' + data.cid ).html( comment_links );
+							self.parent.jQuery( '.pending-count' ).html( moderation_count );
+						} catch ( err ) {
+						}
+						$j( '#status' ).attr( 'class', 'success' );
+						$j( '#message' ).html( wpajaxeditcommentedit.AEC_Saved );
+						parent.jQuery.fn.colorbox.close();
+					}
+				}, 'json' );
 
-	  }
+		}
 	  //Cookie code conveniently stolen from http://www.quirksmode.org/js/cookies.html
 		function createCookie(name,value,days) {
 	    if (days) {
